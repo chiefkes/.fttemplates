4af16dfca30da9e0d8c4144b51cdf773842aa939
chiefkes <max.liefkes@gmail.com>
2021-09-13 00:06:11 +0100

    first commit

---
+++ Skyscanner React Function Component with SCSS/[FTName].jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+const className = require("classNameGen").default(() =>
+  require("./[FTName].scss")
+);
+
+const <FTName | capitalize> = () => {
+  return (
+    //
+    <div></div>
+  );
+}
+
+<FTName | capitalize>.propTypes = {
+
+}
+
+export default <FTName | capitalize>
